6d82cbf4f8ea1981f4604ff74d6782d109d6a454
20wardhaughw <20wardhaughw@jmf6.org>
2026-06-10 12:52:58 +0100

    main window

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
-#Commit Test
+import tkinter as tk
+
+# main window
+class mainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("test")
+        self.geometry("500x500")
+        self.configure( bg= "gray")
+
+
+window = mainWindow()
+
+# classes
+
+# cell used in grid display only, state is bool, True -> alive, False -> dead
+class DisplayCell(tk.Frame):
+    def __init__(self, x: int, y: int, state):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.state = state
+
+        if self.state is None:
+            self.state = False
+
+        self.configure(master= displayGrid, width= 10, height= 10)
+        self.grid(row= self.x, column=self.y)
+
+    # proceedure to update colour based on state
+    def updateColour(self):
+        if self.state:
+            self.configure(bg= "black")
+        else:
+            self.configure(bg= "white")
+
+
+
+
+
+# display grid for displaying the main grid used for the program
+displayGrid = tk.Frame(background= "blue", width= 100, height= 100, master= window)
+displayGrid.columnconfigure(10, minsize= 10)
+displayGrid.rowconfigure(10, minsize= 10)
+displayGrid.pack()
+
+#label1 = tk.Frame(bg= "black", master= displayGrid, width= 10, height= 10)
+#label1.grid(row= 1, column= 2)
+
+cell1 = DisplayCell(1,2, False)
+
+
+window.mainloop()
