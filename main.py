bf2a70192b89dc187eedf1e7d35bdebeff54355e
20wardhaughw <20wardhaughw@jmf6.org>
2026-06-24 13:04:37 +0100

    speed slider

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import tkinter as tk
 
 # endregion
 
-
 # region globals
 
 # width of the grid that is displayed, the real grid has unlimited size
-GridWidth = 20
+GridWidth = 40  # note that higher values tend to lead to lag when resizing
 
 # colour customization of the cells
 AliveCol = "black"
@@ -47,8 +46,8 @@ class DisplayCell(tk.Frame):
         else:
             self.configure(bg=DeadCol)
 
-        # set cell size
-        self.configure(width=10, height=10)
+        # set cell size relative to the grid width
+        self.configure(width=400/GridWidth, height=400/GridWidth)
 
         # set the position in the displayGrid widget
         self.grid(row=self.x, column=self.y)
@@ -64,15 +63,13 @@ class DisplayCell(tk.Frame):
 # endregion
 
 
-# region instantiation
+# region instantiation and widgets
 
 # Instance of the main window used
 window = MainWindow()
 
 # display grid for displaying the main grid used for the program
 displayGrid = tk.Frame(background="black", width=2.5 * GridWidth, height=2.5 * GridWidth, master=window, borderwidth=3)
-
-
 displayGrid.columnconfigure(GridWidth)
 displayGrid.rowconfigure(GridWidth)
 displayGrid.pack(side=tk.RIGHT, padx=35)
@@ -83,6 +80,18 @@ for X in range(GridWidth):
     displayCells.append([])
     for Y in range(GridWidth):
         displayCells[X].append(DisplayCell(X, Y, ((X+Y) % 2 == 0), displayGrid))
+
+
+# speed slider used for the simulation speed
+SimSpeed = tk.DoubleVar()
+speedScale = tk.Scale(master=window, orient=tk.HORIZONTAL, label="speed", variable=SimSpeed, from_=1, to=100)
+
+# places the slider underneath the grid
+speedScale.place(in_=displayGrid, relx=0.4, rely=1, y=50)
+
+# time control widget to contain the start, stop, and step forward buttons
+timeControl = tk.Frame()
+
 
 # endregion
 
