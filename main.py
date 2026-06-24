016065789ffb6cc3d0cc04088f56d6a8618739a9
20wardhaughw <20wardhaughw@jmf6.org>
2026-06-24 12:21:44 +0100

    Display grid and cells working

--- main.py
+++ main.py
@@ -1,67 +1,91 @@
+# region imports
+
 import tkinter as tk
 
+# endregion
+
+
+# region globals
+
+# width of the grid that is displayed, the real grid has unlimited size
+GridWidth = 20
+
+# colour customization of the cells
+AliveCol = "black"
+DeadCol = "white"
+
+
+# endregion
+
+
+# region classes
+
 # main window
-class mainWindow(tk.Tk):
+class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("test")
         self.geometry("500x500")
-        self.configure( bg= "gray")
+        self.configure(bg="gray")
 
-
-
-window = mainWindow()
-
-
-
-# display grid for displaying the main grid used for the program
-displayGrid = tk.Frame(background="blue", width=100, height=100, master=window)
-displayGrid.columnconfigure(10, minsize=10)
-displayGrid.rowconfigure(10, minsize=10)
-displayGrid.pack()
-
-
-
-# classes
 
 # cell used in grid display only, state is bool, True -> alive, False -> dead
 class DisplayCell(tk.Frame):
-    # possible fix: def __init__(self, master, ...)
-    # then super().__init__(master=master)
-    def __init__(self, x: int, y: int, state):
-        super().__init__()
+    def __init__(self, x: int, y: int, state: bool, master):
+        super().__init__(master=master)
         self.x = x
         self.y = y
         self.state = state
-        
+
         # used to set the state to dead by default
         if self.state is None:
             self.state = False
 
-
-        self.configure(width= 10, height= 10)
-
-
-        # problem
-        self.master(displayGrid)
-        self.grid(row= self.x, column=self.y)
-
-    # proceedure to update colour based on state
-    def updateColour(self):
+        # set initial colour
         if self.state:
-            self.configure(bg= "black")
+            self.configure(bg=AliveCol)
         else:
-            self.configure(bg= "white")
+            self.configure(bg=DeadCol)
+
+        # set cell size
+        self.configure(width=10, height=10)
+
+        # set the position in the displayGrid widget
+        self.grid(row=self.x, column=self.y)
+
+    # procedure to update colour based on state
+    def update_colour(self):
+        if self.state:
+            self.configure(bg=AliveCol)
+        else:
+            self.configure(bg=DeadCol)
 
 
+# endregion
 
 
+# region instantiation
+
+# Instance of the main window used
+window = MainWindow()
+
+# display grid for displaying the main grid used for the program
+displayGrid = tk.Frame(background="black", width=2.5 * GridWidth, height=2.5 * GridWidth, master=window, borderwidth=3)
 
 
+displayGrid.columnconfigure(GridWidth)
+displayGrid.rowconfigure(GridWidth)
+displayGrid.pack(side=tk.RIGHT, padx=35)
 
-#label1 = tk.Frame(bg= "black", master= displayGrid, width= 10, height= 10)
-#label1.grid(row= 1, column= 2)
-cell1 = DisplayCell(1,2,False)
+# creation of the 2d array for the display cell objects in the grid
+displayCells = []
+for X in range(GridWidth):
+    displayCells.append([])
+    for Y in range(GridWidth):
+        displayCells[X].append(DisplayCell(X, Y, ((X+Y) % 2 == 0), displayGrid))
+
+# endregion
 
 
+# main loop for the simulation
 window.mainloop()
